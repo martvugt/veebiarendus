88cc5e21530dc7d07524bbd0220a581d8bedb273
martvugt <116370405+martvugt@users.noreply.github.com>
2022-11-21 16:13:09 +0200

    Loeng 09 värskendus

---
+++ loeng08/harjutus.js
@@ -0,0 +1,15 @@
+let varv
+varv = prompt('Sisesta enda lemmikvärv')
+alert(varv + ' on ilus värv')
+
+let blokk
+//console.log(nimi)
+blokk = document.getElementById('valjund')
+//blokk.innerText = 'Tere, ' + nimi
+//blokk.innerText = 'Tere, ' + nimi
+
+for (let index = 1; index < 11; index++) {
+    console.log('Number: ' + index)
+}
+
+console.log('Programm lõpetas')
