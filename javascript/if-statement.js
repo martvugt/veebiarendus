b7507bef9cdfdb77fb3b7c96cd2f8824503d3d98
martvugt <116370405+martvugt@users.noreply.github.com>
2022-11-28 17:23:10 +0200

    Loeng 12

--- javascript/if-statement.js
+++ javascript/if-statement.js
@@ -1,11 +1,11 @@
-//Tee nii et konsoolile trükitakse:
-//
-//let fruit = 'orange' (let väärtust saab muuta, const väärtust muuta ei saa)
-const fruit = 'orange'
-//console.log(fruit.lenght)
+//Tee nii et komnsoolile trükitakse:
+// "The fruit name has more than five characters." kui puuvilja nimi on pikem kui viis sümbollit
+// Vastasel juhul trükitakse:
+// "The fruit name has five characters or less." 
 
-if (fruit.length > 5) {
-    console.log('The fruit name has more than five characters.')
-  } else {
+const fruit = 'orange'
+if (fruit.length > 5) {  
+    console.log('The fruit name has more than five characters.')  
+  } else {  
     console.log('The fruit name has five characters or less.')
-  }
+  }  
