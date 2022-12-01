8b7d70a48820bafea60fed5b79f36a1f683b44a1
martvugt <116370405+martvugt@users.noreply.github.com>
2022-12-01 20:00:05 +0200

    01-12-2022

--- loeng13/todo.js
+++ loeng13/todo.js
@@ -1,34 +1,80 @@
 const ylesanne1 = {
     kirjeldus: 'Hommikvõimlemine',
     tehtud: true,
-    olulisus: 2
+    olulisus: 1
 }
 const ylesanne2 = {
     kirjeldus: 'Praadida mune',
     tehtud: false,
-    olulisus: 4
+    olulisus: 2
 }
 const ylesanne3 = {
     kirjeldus: 'Jaluta koera',
     tehtud: false,
-    olulisus: 5
+    olulisus: 3
 }
 
 const ylesanded = [ylesanne1, ylesanne2, ylesanne3]
+function valmistaTahekesed(olulisus) {
+    let tagasta = ''
+    for (let i = 1; i <= olulisus; i++) {
+        tagasta += `<span class="material-symbols-outlined">
+grade
+</span>`
 
-function naitaYlesannet(ylesanne) {
- const valjund = document.getElementById('valjund')
- let isChecked = ""
- if (ylesanne.tehtud) {
-    isChecked = "checked=true"
- }
- valjund.innerHTML += `
+    }
+    return tagasta
+}
+
+function naitaYlesannet(ylesanne, index) {
+    const valjund = document.getElementById('valjund')
+    let isChecked = ""
+    if (ylesanne.tehtud) {
+        isChecked = "checked=true"
+    }
+    const tahekesed = valmistaTahekesed(ylesanne.olulisus)
+
+    valjund.innerHTML += `
     <div class="kirjeldus">
-        <input type="checkbox" ${isChecked}>
+        <input type="checkbox" onclick="MuudaCheckboxi(2)" ${isChecked}>
         ${ylesanne.kirjeldus}
+        ${tahekesed}
     </div>
  `
 }
 
-for (y of ylesanded)
-naitaYlesannet(y)
+function MuudaCheckboxi(index) {
+    console.log("Muutsin checkboxi")
+    ylesanded[index].tehtud = !ylesanded[index].olulisus
+}
+
+function lisaYlesanne() {
+    //loe ekraani input elementide väärtused
+    const nimetus = document.getElementById('uus_ylesanne').value
+    const olulisus = parseInt (document.getElementById('olulisus').value)
+    console.log(nimetus, olulisus)
+    //loo ülesande objekt
+    const uusYlesanne = {
+        kirjeldus: nimetus,
+        tehtud: false,
+        olulisus: olulisus
+        //olulisus: olulisus
+    }
+    console.log(uusYlesanne)
+    //lisa uus ülesande objekt ylesannete massiivi
+    ylesanded.push(uusYlesanne)
+    console.log(ylesanded)
+    //joonista(renderda) uuesti väljund
+    naitaKoguValjund()
+}
+    function naitaKoguValjund() {
+document.getElementById('valjund').innerHTML = ''
+let i = 0;
+        for (y of ylesanded)
+    naitaYlesannet(y, i)
+    i+= 1
+
+    }
+
+
+naitaKoguValjund()
