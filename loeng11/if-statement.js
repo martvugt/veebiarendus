a873ea619090e180ba2e31b9b4f8ff46eff23ee7
martvugt <116370405+martvugt@users.noreply.github.com>
2022-12-01 17:18:16 +0200

    uuendus01-12-2022

---
+++ loeng11/if-statement.js
@@ -0,0 +1,13 @@
+//Tee nii et komnsoolile trükitakse:
+// "The fruit name has more than five characters." kui puuvilja nimi on pikem kui viis sümbollit
+// Vastasel juhul trükitakse:
+// "The fruit name has five characters or less." 
+// console.log('hello')
+
+
+const fruit = 'orange'
+if (fruit.length > 5) {  
+    console.log('The fruit name has more than five characters.')  
+  } else {  
+    console.log('The fruit name has five characters or less.')
+  }  
